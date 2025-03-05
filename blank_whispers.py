4460baba77d5bf743c7318366de80c8a1dfd8d7b
Ardeleanu Vlad-George <ardeleanu_vlad@ymail.com>
2025-03-05 15:03:44 +0200

    implemented the 'xtra_jumps' mechanism

--- blank_whispers.py
+++ blank_whispers.py
@@ -12,9 +12,22 @@ with open(sys.argv[1], 'r') as src:
     for line in src:
         code += line
 
+xtra_jumps = 0
+
 for char_idx in range(len(code)):
+    if xtra_jumps:
+        xtra_jumps-=1
+        continue
     if code[char_idx] in " \t\n":
         continue
     print(code[char_idx], end='')
+
+    # the 'xtra_jumps' variable will be used to jump over 
+    # chars that don't need to further be analyzed
+    # this is useful for languages where in order 
+    # to start a 'one-line' comment you need a sequence
+    # of multiple chars
+    if code[char_idx] == 'm':
+        xtra_jumps = 2
 
 print()
