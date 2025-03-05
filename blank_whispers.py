ce629fab052bc40bf3fd8a53491fb304bf29e99b
Ardeleanu Vlad-George <ardeleanu_vlad@ymail.com>
2025-03-05 14:29:04 +0200

    Can read and print input source code

--- blank_whispers.py
+++ blank_whispers.py
@@ -5,4 +5,12 @@ if len(sys.argv) != 2:
     print("Please give just one CLA, otherwise the program can't work")
     exit(-1)
 
-print("Normal behaviour")
+code = ''
+
+with open(sys.argv[1], 'r') as src:
+    src = src.readlines()
+    for line in src:
+        code += line
+
+
+print(code, end='')
