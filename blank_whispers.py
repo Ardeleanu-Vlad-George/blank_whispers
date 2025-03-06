234bdc45dca63853b2d443ed678504fd4bb21b6b
Ardeleanu Vlad-George <ardeleanu_vlad@ymail.com>
2025-03-06 14:25:56 +0200

    added comment detection mechanism, refactored code for efficiency

--- blank_whispers.py
+++ blank_whispers.py
@@ -1,9 +1,56 @@
 #!/usr/bin/python3
+'''
+    In this part we define the constants the program needs in order to work
+'''
+def Python_comment_detection(idx, src):
+    if src[idx] == '#' and src[idx+1] != '!':
+        return 2
+    return 0
+
+Python_rules = ('Python script', 'py', Python_comment_detection)
+
+def C_comment_detection(idx, src):
+    if src[idx] == '/' and src[idx+1] == '/':
+        return 2
+    return 0
+
+C_rules      = ('C source file', 'c', C_comment_detection)
+
+# Define more rules here, then add them to 'all_rules'
+
+all_rules = (Python_rules, C_rules)
+
+
+'''
+    Entering the 'main' body of the program
+'''
 import sys
 
 if len(sys.argv) != 2:
     print("Please give just one CLA, otherwise the program can't work")
     exit(-1)
+
+extn_idx = 0
+
+for extn_idx in range(len(sys.argv[1])):
+    if sys.argv[1][extn_idx] == '.':
+        break
+
+extn = sys.argv[1][extn_idx+1:]
+
+func = None
+name = None
+
+for rule in all_rules:
+    if extn in rule[1:-1]:
+        name = rule[0]
+        func = rule[-1]
+
+if func is None:
+    print(extn, "is an unknown file extension. Cannot detect it's type for processing.")
+    exit(-1)
+
+print("Detected next file type:", name)
 
 code = ''
 
@@ -15,14 +62,6 @@ with open(sys.argv[1], 'r') as file:
 xtra_jumps = 0
 line_idx = 1
 colm_idx = 0
-extn_idx = 0
-
-for extn_idx in range(len(sys.argv[1])):
-    if sys.argv[1][extn_idx] == '.':
-        break
-
-extn = sys.argv[1][extn_idx+1:]
-print("File extension:", extn)
 
 for char_idx in range(len(code)):
     colm_idx+=1
@@ -41,8 +80,8 @@ for char_idx in range(len(code)):
     # this is useful for languages where in order 
     # to start a 'one-line' comment you need a sequence
     # of multiple chars
-    if code[char_idx] == 'm':
+    xtra_jumps=func(char_idx, code)
+    if xtra_jumps:
         print("Comment detected at: (%d, %d)" % (line_idx, colm_idx))
-        xtra_jumps=2
 
 print("Program ended")
